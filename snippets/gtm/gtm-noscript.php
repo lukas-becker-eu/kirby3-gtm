48049a96487ddd0675e97741f82b4b43d2936227
Lukas Becker <lukas@candyblue.agency>
2020-05-27 21:34:46 +0200

    Update

--- snippets/gtm/gtm-noscript.php
+++ snippets/gtm/gtm-noscript.php
@@ -1,6 +1,6 @@
 <?php if(option('candyblue.kirby3-gtm') && gtmId()): ?>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $site->gtmId() ?>"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= gtmId() ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <?php endif ?>
