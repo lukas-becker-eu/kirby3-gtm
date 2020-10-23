162bbe105f0aa41bb84b874ff736420f20d6f1d1
Lukas Becker <lukas@candyblue.agency>
2020-10-23 19:26:37 +0200

    Update

--- lib/gtmId.php
+++ lib/gtmId.php
@@ -1,5 +1,9 @@
 <?php
 
-function gtmId() {
-  return kirby()->site()->gtmId()->isNotEmpty() ? kirby()->site()->gtmId()->value() : option('candyblue.kirby3-gtm.id');
+namespace candyblue\gtm;
+
+class gtm {
+  function gtmId() {
+    return kirby()->site()->gtmId()->isNotEmpty() ? kirby()->site()->gtmId()->value() : option('candyblue.kirby3-gtm.id');
+  }
 }
