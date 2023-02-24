abffb7c814d9bd6b124d746bdf1b34638f7950c7
Lukas Becker <lukas@lukas-becker.eu>
2023-02-24 12:32:01 +0100

    Update gtm-noscript.php

--- snippets/gtm-noscript.php
+++ snippets/gtm-noscript.php
@@ -1,6 +1,6 @@
-<?php if(!empty(option('candyblue.kirby3-gtm.id'))): ?>
+<?php if(!empty(option('lukasbecker.kirby3-gtm.id'))): ?>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= option('candyblue.kirby3-gtm.id') ?>"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= option('lukasbecker.kirby3-gtm.id') ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <?php endif ?>
