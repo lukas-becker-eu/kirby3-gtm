316089dfab651c3252ed7a4ab0c41920afdb08e9
Lukas Becker <lukas@lukas-becker.eu>
2023-02-24 12:31:40 +0100

    Update gtm-dataLayer.php

--- snippets/gtm-dataLayer.php
+++ snippets/gtm-dataLayer.php
@@ -1,5 +1,5 @@
 <?php if(!empty($dataLayer)): ?>
 <script>
-  <?= option('candyblue.kirby3-gtm.dataLayer');?>.push(<?= json_encode($dataLayer) ?>);
+  <?= option('lukasbecker.kirby3-gtm.dataLayer');?>.push(<?= json_encode($dataLayer) ?>);
 </script>
 <?php endif ?>
