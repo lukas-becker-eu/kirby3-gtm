b422ee79b7b1dc5d49321f9e147befa2e65556e8
Lukas Becker <lukas@lukas-becker.eu>
2023-11-19 15:53:07 +0100

    Update gtm-dataLayer.php

--- snippets/gtm-dataLayer.php
+++ snippets/gtm-dataLayer.php
@@ -1,9 +1,20 @@
-<?php if(!empty($dataLayer)): ?>
-<?php if(!empty(option('lukasbecker.kirby3-gtm.scriptAttr.name'))): ?>
-<script <?= Html::attr(option('lukasbecker.kirby3-gtm.scriptAttr.name'), option('lukasbecker.kirby3-gtm.scriptAttr.value')) ?>>
-<?php else: ?>
-<script>
-<?php endif ?>
-  <?= option('lukasbecker.kirby3-gtm.dataLayer');?>.push(<?= json_encode($dataLayer) ?>);
-</script>
+<?php if (!empty($slot)) : ?>
+
+  <?php
+  $dataLayer = option('lukasbecker.kirby3-gtm.dataLayer', 'dataLayer');
+  $attrName = option('lukasbecker.kirby3-gtm.scriptAttr.name');
+  $attrValue = option('lukasbecker.kirby3-gtm.scriptAttr.value');
+  ?>
+
+  <?php if (!empty($attrName) && !empty($attrValue)) : ?>
+    <script <?= Html::attr($attrName, $attrValue) ?>>
+      window.<?= $dataLayer ?> = window.dataLayer || [];
+      window.<?= $dataLayer ?>.push(<?= $slot ?>);
+    </script>
+  <?php else : ?>
+    <script>
+      window.<?= $dataLayer ?> = window.dataLayer || [];
+      window.<?= $dataLayer ?>.push(<?= $slot ?>);
+    </script>
+  <?php endif ?>
 <?php endif ?>
