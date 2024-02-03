484904d02b9e46a72b3d459d1d2bc020132075eb
Lukas Becker <lukas@lukas-becker.eu>
2024-02-03 18:21:02 +0100

    Fix intendation

--- snippets/gtm-consent-mode.php
+++ snippets/gtm-consent-mode.php
@@ -1,30 +1,29 @@
 <!-- Google Consent Mode -->
 <?php if (!empty(option('lukasbecker.kirby3-gtm.scriptAttr.name')) && !empty(option('lukasbecker.kirby3-gtm.scriptAttr.value'))) : ?>
     <script <?= Html::attr(option('lukasbecker.kirby3-gtm.scriptAttr.name'), option('lukasbecker.kirby3-gtm.scriptAttr.value')) ?>>
-        <?php elseif (!empty(option('lukasbecker.kirby3-gtm.scriptAttr.name'))) : ?>
-                <
-                script <?= Html::attr(option('lukasbecker.kirby3-gtm.scriptAttr.name')) ?> >
-            <?php else : ?> <
-                script >
-            <?php endif ?>
-            window.<?= option('lukasbecker.kirby3-gtm.dataLayer', 'dataLayer') ?> = window.dataLayer || [];
+<?php elseif (!empty(option('lukasbecker.kirby3-gtm.scriptAttr.name'))) : ?>
+    <script <?= Html::attr(option('lukasbecker.kirby3-gtm.scriptAttr.name')) ?> >
+<?php else : ?>
+    <script>
+<?php endif ?>
+        window.<?= option('lukasbecker.kirby3-gtm.dataLayer', 'dataLayer') ?> = window.dataLayer || [];
 
-            function gtag() {
-                <?= option('lukasbecker.kirby3-gtm.dataLayer', 'dataLayer') ?>.push(arguments);
-            }
+        function gtag() {
+            <?= option('lukasbecker.kirby3-gtm.dataLayer', 'dataLayer') ?>.push(arguments);
+        }
 
-            gtag("consent", "default", {
-                ad_storage: "<?= option('lukasbecker.kirby3-gtm.ad_storage', 'denied') ?>",
-                ad_user_data: "<?= option('lukasbecker.kirby3-gtm.ad_user_data', 'denied') ?>",
-                ad_personalization: "<?= option('lukasbecker.kirby3-gtm.ad_personalization', 'denied') ?>",
-                analytics_storage: "<?= option('lukasbecker.kirby3-gtm.analytics_storage', 'denied') ?>",
-                functionality_storage: "<?= option('lukasbecker.kirby3-gtm.functionality_storage', 'denied') ?>",
-                personalization_storage: "<?= option('lukasbecker.kirby3-gtm.personalization_storage', 'denied') ?>",
-                security_storage: "<?= option('lukasbecker.kirby3-gtm.security_storage', 'granted') ?>",
-                wait_for_update: <?= option('lukasbecker.kirby3-gtm.wait_for_update', 500) ?>,
-            });
+        gtag("consent", "default", {
+            ad_storage: "<?= option('lukasbecker.kirby3-gtm.ad_storage', 'denied') ?>",
+            ad_user_data: "<?= option('lukasbecker.kirby3-gtm.ad_user_data', 'denied') ?>",
+            ad_personalization: "<?= option('lukasbecker.kirby3-gtm.ad_personalization', 'denied') ?>",
+            analytics_storage: "<?= option('lukasbecker.kirby3-gtm.analytics_storage', 'denied') ?>",
+            functionality_storage: "<?= option('lukasbecker.kirby3-gtm.functionality_storage', 'denied') ?>",
+            personalization_storage: "<?= option('lukasbecker.kirby3-gtm.personalization_storage', 'denied') ?>",
+            security_storage: "<?= option('lukasbecker.kirby3-gtm.security_storage', 'granted') ?>",
+            wait_for_update: <?= option('lukasbecker.kirby3-gtm.wait_for_update', 500) ?>,
+        });
 
-            gtag("set", "ads_data_redaction", <?= option('lukasbecker.kirby3-gtm.ads_data_redaction', 'true') ?>);
-            gtag("set", "url_passthrough", <?= option('lukasbecker.kirby3-gtm.ads_data_redaction', 'false') ?>);
+        gtag("set", "ads_data_redaction", <?= option('lukasbecker.kirby3-gtm.ads_data_redaction', 'true') ?>);
+        gtag("set", "url_passthrough", <?= option('lukasbecker.kirby3-gtm.ads_data_redaction', 'false') ?>);
     </script>
-    <!-- End Google Consent Mode -->
+<!-- End Google Consent Mode -->
